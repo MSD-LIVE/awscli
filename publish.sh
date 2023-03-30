850f3975babecaadfb4cc9ee3ff394b620e2e767
Devin McAllester <devin.mcallester@pnnl.gov>
2023-03-29 19:18:57 -0600

    adds tar ball for AWS CLI version 2.11.5

--- publish.sh
+++ publish.sh
@@ -1,13 +1,21 @@
 # /user/env/bin bash
+set -e
+
+if [ "$1" = "" ]
+then
+  echo "Usage: $0 <AWS CLI version>"
+  exit 1
+fi
+
 AWS_CLI_VERSION=$1
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
-# rm -rf ./aws-cli
-# git clone --depth 1 --branch $AWS_CLI_VERSION https://github.com/aws/aws-cli.git
+rm -rf ./aws-cli
+git clone --depth 1 --branch $AWS_CLI_VERSION https://github.com/aws/aws-cli.git
 
-# cd aws-cli
-# pip install -r requirements.txt
-# flit build
+cd aws-cli
+pip install -r requirements.txt
+flit build
 
 cp dist/awscli-${AWS_CLI_VERSION}.tar.gz ../
 
